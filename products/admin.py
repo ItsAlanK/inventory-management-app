68d3c21e48971a68a489b30e1a917c455e733125
ItsAlanK <alankinsella1993@gmail.com>
2022-06-08 14:59:42 +0100

    Create models and admin menus for products, inventory and locations. Basic workflow possible through admin.

--- products/admin.py
+++ products/admin.py
@@ -1,3 +1,61 @@
 from django.contrib import admin
+from .models import Product, Location, Inventory
 
-# Register your models here.
+
+class InventoryAdminInline(admin.TabularInline):
+
+    model = Inventory
+
+    list_display = (
+        'product',
+        'quantity',
+        'location'
+    )
+
+
+class ProductAdmin(admin.ModelAdmin):
+
+    inlines = (InventoryAdminInline,)
+
+    list_display = (
+        'name',
+        'SKU',
+        'weight',
+        'value',
+        'notes'
+    )
+
+    ordering = ('name',)
+
+
+class LocationAdmin(admin.ModelAdmin):
+
+    inlines = (InventoryAdminInline,)
+
+    list_display = (
+        'name',
+        'address_line_1',
+        'address_line_2',
+        'address_line_3',
+        'county',
+        'postcode',
+        'country',
+    )
+
+    ordering = ('name',)
+
+
+class InventoryAdmin(admin.ModelAdmin):
+
+    model = Inventory
+
+    list_display = (
+        'product',
+        'quantity',
+        'location'
+    )
+
+
+admin.site.register(Product, ProductAdmin)
+admin.site.register(Location, LocationAdmin)
+admin.site.register(Inventory, InventoryAdmin)
--- products/models.py
+++ products/models.py
@@ -1,3 +1,49 @@
 from django.db import models
 
-# Create your models here.
+
+class Product(models.Model):
+    """
+    Model for each products details
+    """
+    SKU = models.IntegerField(default=0)
+    name = models.CharField(max_length=254)
+    weight = models.IntegerField(default=0)
+    value = models.DecimalField(max_digits=8, decimal_places=2)
+    notes = models.TextField(null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Location(models.Model):
+    """
+    Model for each locations details
+    """
+    name = models.CharField(max_length=254)
+    address_line_1 = models.CharField(max_length=254)
+    address_line_2 = models.CharField(max_length=254, null=True, blank=True)
+    address_line_3 = models.CharField(max_length=254, null=True, blank=True)
+    county = models.CharField(max_length=254, default='county')
+    postcode = models.CharField(max_length=254, default='postcode')
+    country = models.CharField(max_length=254, default='country')
+
+    def __str__(self):
+        return self.name
+
+
+class Inventory(models.Model):
+    """
+    Model which tracks inventory for each product
+    and assigns its location.
+    """
+
+    class Meta:
+        """ Adjust plural of model name in admin """
+        verbose_name_plural = 'Inventory'
+
+    product = models.ForeignKey('Product', on_delete=models.CASCADE)
+    quantity = models.IntegerField(default=0)
+    location = models.ForeignKey('Location', on_delete=models.PROTECT)
+
+    def __str__(self):
+        return self.product.name
