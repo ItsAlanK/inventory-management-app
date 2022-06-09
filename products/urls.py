fc88b74a34048752c6d376154989124a6b92df08
ItsAlanK <alankinsella1993@gmail.com>
2022-06-09 20:33:42 +0100

    Add create locations page, finished add product page and start edit product view and template. Currently displays form to edit product details and list on inventory with manage buttons, no submission yet.

--- products/urls.py
+++ products/urls.py
@@ -5,5 +5,7 @@ from . import views
 urlpatterns = [
     path('', views.index, name='home'),
     path('add-product', views.add_product, name='add_product'),
+    path('edit/<int:product_id>/', views.edit_product, name='edit_product'),
     path('add-location', views.add_location, name='add_location'),
+    path('locations', views.locations, name='locations'),
 ]
--- products/views.py
+++ products/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render, redirect, reverse
+from django.shortcuts import render, redirect, reverse, get_object_or_404
+from django.forms import formset_factory
 from .models import Product, Location, Inventory
 from .forms import ProductForm, InventoryForm, LocationForm
 
@@ -49,6 +50,31 @@ def add_product(request):
     return render(request, template, context)
 
 
+def edit_product(request, product_id):
+    """
+    Edit product details and inventory information
+    """
+    product = get_object_or_404(Product, pk=product_id)
+    product_form = ProductForm(instance=product)
+
+    inventory = Inventory.objects.all()
+    product_inventory = []
+
+    for i in inventory:
+        if i.product == product:
+            product_inventory.append(i)
+
+    template = 'products/edit-product.html'
+
+    context={
+            'product_form': product_form,
+            'product': product,
+            'product_inventory': product_inventory,
+            }
+
+    return render(request, template, context)
+
+
 def add_location(request):
     """ Add a location """
 
@@ -69,4 +95,20 @@ def add_location(request):
     }
 
     return render(request, template, context)
+
+
+def locations(request):
+    """
+    View to handle displaying location list
+    """
+
+    locations = Location.objects.all()
+    inventory = Inventory.objects.all()
+
+    context = {
+        'locations': locations,
+        'inventory': inventory,
+    }
+
+    return render(request, 'products/locations.html', context)
 
